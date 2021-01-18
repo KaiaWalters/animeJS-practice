cd36ec1ae15067b15da2418880b30f0cb2d10bba
Kaia <kaia.cwalters@gmail.com>
2021-01-18 11:40:48 -0500

    first commit

---
+++ anime.js
@@ -0,0 +1,10 @@
+var animation = anime({
+  target: '.square',
+  translateX: 100,
+  borderRadius: 50,
+  // Property Parameters
+  duration: 2000,
+  easing: 'linear',
+  // Animation Parameters
+  direction: 'alternate'
+});
